e69a7d152de5e6e7b9ea4868ee02717977a7dfe4
humanbydefinition <hello@textmode.art>
2026-07-29 22:59:25 +0200

    refactor: use shared GLSL build tooling

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineTextmodeLibrary } from '@textmode/vite-config';
+import { textmodeGlsl } from '@textmode/vite-plugin-glsl';
 
-import { createGLSLPlugin } from './vite-plugins/glsl-minify';
+import { publicShaderSymbols } from './vite-plugins/glsl-public-symbols';
 
 export default defineTextmodeLibrary({
 	globalName: 'textmodeFilters',
-	plugins: [createGLSLPlugin()],
+	plugins: [
+		textmodeGlsl({
+			shaderRoots: ['src/shaders'],
+			includeRoots: ['src/shaders'],
+			publicSymbols: publicShaderSymbols,
+			hostIdentifiers: {
+				sourceRoots: ['src'],
+			},
+		}),
+	],
 });
